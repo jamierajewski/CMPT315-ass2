07482fb19de6784c1db308375d1bf8cb8efbdadc
jamie-rajewski <jamie.rajewski1@gmail.com>
2019-03-01 18:40:24 -0700

    Adjusted directory structure to be in line with Nicks; add tsconfig

--- dist/querySelectorExample.ts
+++
@@ -1,6 +0,0 @@
-var current = document.querySelector('#country');
-
-var newElement = document.createElement('newCountry');
-newElement.innerHTML = '<input type="text" id="country" name="country" placeholder="Your country...">'
-
-current.parentNode.replaceChild(newElement, current)
