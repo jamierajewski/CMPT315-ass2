27e4e1bdf453a7bad425b46cd02a93fc5d0f313a
jamie-rajewski <jamie.rajewski1@gmail.com>
2019-03-02 13:14:44 -0700

    Basic login page complete; successfully validates login ID

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-let x = "hello";
+// Assignment #2 - CMPT315 - Jamie Rajewski - 3020090
+
+let loginID = "";
+
+let clickSubmit = (evt: Event): void => {
+
+    let submitText = <HTMLInputElement>document.querySelector("#login-textbox");
+    let token = submitText.value;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "/api/v1/people/presenters");
+    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+    xhr.send();
+
+    xhr.onload = function(){
+        if (xhr.status == 200){
+            // Success; load template for next page and render it with the content 
+            //received from this request
+            alert(xhr.responseText);
+        }
+        else {
+            alert("Invalid login ID, please try again");
+        }
+    }
+}
+
+let attachListener = (): void => {
+    let submitBtn = <HTMLElement>document.querySelector("#login-submit");
+    submitBtn.addEventListener("click", clickSubmit);
+}
+
+// This always starts as the login page
+window.onload = (): void => {
+    attachListener();
+}
